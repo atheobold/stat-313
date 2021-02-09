329b401d42aae7f689bbe62e6d0eb59c288ad6b0
Allison Theobold <atheobol@calpoly.edu>
2021-02-08 18:47:20 -0800

    tuesday exams

--- Midterm Exam/random_questions.R
+++ Midterm Exam/random_questions.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 set.seed(1234)
 
-monday_students <- c("Ryan", "Alex", "Reed", "Amrit", "Mia", "Thomas", 
+monday_students <- c("Ryan", "Alex", "Reed", "Amrit", "Mia", "Thomas B.", 
                      "Katarina")
 
 monday_q1 <- rdunif(length(monday_students), a = 1, b = 6)
@@ -9,4 +9,14 @@ monday_q2 <- rdunif(length(monday_students), a = 1, b = 6)
 
 monday <- tibble(students = monday_students, question1 = monday_q1, 
                  question2 = monday_q2)  
+
+
+tuesday_students <- c("Thomas S.", "Jade", "Rachel K.", "Emma", 
+                      "Sam C.", "Sam W.")
+
+tuesday_q1 <- rdunif(length(tuesday_students), a = 1, b = 6)
+tuesday_q2 <- rdunif(length(tuesday_students), a = 1, b = 6)
+
+tuesday <- tibble(students = tuesday_students, question1 = tuesday_q1, 
+                 question2 = tuesday_q2)  
 
