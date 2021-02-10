13074881733f7bd1528d57790547c49e0e00bcf9
atheobold <atheobol@calpoly.edu>
2021-02-09 20:09:20 -0800

    exam function!

--- Midterm Exam/random_questions.R
+++ Midterm Exam/random_questions.R
@@ -1,6 +1,16 @@
 library(tidyverse)
 set.seed(1234)
 
+exam_function <- function(x, a = 1, end){
+  n = length(x)
+  q1 <- rdunif(length(x), a = a, b = end)
+  q2 <- rdunif(length(x), a = a, b = end)
+  
+  tibble(students = x,
+         question1 = q1, 
+         question2 = q2)  
+}
+
 monday_students <- c("Ryan", "Alex", "Reed", "Amrit", "Mia", "Thomas B.", 
                      "Katarina")
 
@@ -20,3 +30,8 @@ tuesday_q2 <- rdunif(length(tuesday_students), a = 1, b = 6)
 tuesday <- tibble(students = tuesday_students, question1 = tuesday_q1, 
                  question2 = tuesday_q2)  
 
+wednesday_students <- c("Mackenzie", "Emily", "Shannon", 
+                        "Matthew", "Emma", "Abby", "Kevin", 
+                        "Jana", "Wiyi")
+
+wednesday <- exam_function(wednesday_students, end = 6)
