e4c0ac85d6809849f7c7b5bb3f60c41df7b3abfe
atheobold <atheobol@calpoly.edu>
2021-03-15 08:35:53 -0700

    final exams

---
+++ Exams/random_questions.R
@@ -0,0 +1,30 @@
+
+library(tidyverse)
+library(googlesheets4)
+
+set.seed(1234)
+
+final_exam <- read_sheet("https://docs.google.com/spreadsheets/d/1a9Hl7gT38i8Wk0hwFHcaQ9Ne4_NmhcoOoWfP1c-pm4I/edit#gid=0")
+
+monday <- final_exam %>% 
+  select(Monday) %>% 
+  filter(Monday != "NULL")
+
+tuesday <- final_exam %>% 
+  select(Tuesday) %>% 
+  filter(Tuesday != "NULL")
+
+wednesday <- final_exam %>% 
+  select(Wednesday) %>% 
+  filter(Wednesday != "NULL")
+
+exam_questions <- function(x, bank){
+  q <- rdunif(1, a = 1, b = bank)
+  return(q)
+}
+
+
+monday_exams <- monday %>% 
+  mutate(q1 = map_dbl(Monday, exam_questions, bank = 5), 
+         q2 = map_dbl(Monday, exam_questions, bank = 5)
+         )
